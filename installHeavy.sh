bf728ece1d7cf364a333cab500ce6f00e9151026
mikew-heavyai <michael.waldrop@heavy.ai>
2024-03-28 07:56:43 -0400

    jupyter volume location updates

--- installHeavy.sh
+++ installHeavy.sh
@@ -319,9 +319,8 @@ c.DockerSpawner.notebook_dir = notebook_dir
 
 # Mount the real user's Docker volume on the host to the notebook user's
 # notebook directory in the container
-c.DockerSpawner.volumes = {#"jupyterhub-user-{username}": notebook_dir, 
-                        #"jupyterhub-shared": "/home/jovyan/work/shared",
-                        "/home/ubuntu/jupyterData": "/home/jovyan/work"}
+c.DockerSpawner.volumes = {"/home/ubuntu/jupyterData": "/home/jovyan/work",
+                        "/var/lib/heavyai" : "/var/lib/heavyai"}
 
 # Remove containers once they are stopped
 c.DockerSpawner.remove = True
