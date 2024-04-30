36e51b965eeb1be76f37b013500c46fb34b9d7d6
mikew-heavyai <michael.waldrop@heavy.ai>
2024-04-30 11:30:56 -0400

    initial setup

--- nvidiaUninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Remove the nvidia drivers installed with nvidiaSetup.sh
-
-sudo apt purge 'nvidia*' 'libnvidia*' 'cuda*' 'libcuda*'
-sudo apt --fix-broken -y install
-sudo apt autoremove -y
-
