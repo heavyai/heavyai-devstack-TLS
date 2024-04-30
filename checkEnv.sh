c9779ab36f2fb68c9379da42908b59354d1b8ba7
mikew-heavyai <michael.waldrop@heavy.ai>
2024-04-30 17:16:04 -0400

    setup changes

--- checkEnv.sh
+++ checkEnv.sh
@@ -1,86 +1,90 @@
 #!/bin/bash
 
-os_packages=(
-    "docker.io" 
-    "docker-compose"
-    "jq"
-    "xmlstarlet"
-    )
+# Define colors for output
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
+os_packages=("docker" "xmlstarlet")
 missing_packages=()
+UPDATE_DONE=0
+
+echo -e "${BLUE}Checking for required packages...${NC}"
+for package in "${os_packages[@]}"; do
+    if dpkg -l | grep -q "^ii\s\+$package\s"; then
+        echo -e "$package:   [${GREEN}Installed${NC}]"
+    else
+        echo -e "$package:   [${RED}Not Installed${NC}]"
+        missing_packages+=("$package")
+    fi
+done
 
 installPackage(){
-
     PACKAGE_NAME=$1
     echo "Would you like to install $PACKAGE_NAME [y/n]"
     read answer
 
     case $answer in
-    [Yy]* ) echo "Installing $package";
-        if [ $UPDATE_DONE -eq 0 ]; then
-            echo "Updating APT repository"
-            sudo apt-get update
-            UPDATE_DONE=1   
-        fi
-            sudo apt-get install $PACKAGE_NAME
-        ;;
-
-    [Nn]* ) echo "Skipping $package";;
-
-    * ) echo "Invalid choice.";; 
+        [Yy]* )
+            echo "Installing $PACKAGE_NAME";
+            if [ $UPDATE_DONE -eq 0 ]; then
+                echo "Updating APT repository"
+                sudo apt-get update
+                UPDATE_DONE=1
+            fi
+            sudo apt-get install -y $PACKAGE_NAME
+            ;;
+        [Nn]* )
+            echo "Skipping $PACKAGE_NAME"
+            ;;
+        * )
+            echo "Invalid choice."
+            ;;
     esac
 }
-
-packageStatus(){
-    PACKAGE_NAME=$1
-    if dpkg -l | grep -q "^ii\s\+$PACKAGE_NAME\s"; then
-        return 1
-    else
-        return 0
-    fi
-}
-
-displayStatus(){
-    for package in "${os_packages[@]}"; do
-        packageStatus $package
-        if [ $? -eq 0 ]; then
-            status="\033[0;31mMissing\033[0m"
-            missing_packages+=("$package")
-        else
-            status="\033[0;32mLoaded\033[0m"
-        fi
-        echo -e $package ":   [" $status "]"
-    done
-}
-
-getGPUInfo(){
-
-# Fetch GPU information in XML format
-xml_output=$(nvidia-smi -q -x| sed '/<!DOCTYPE/d')
-echo "here"
-# Parse XML to get GPU name, driver version, and temperature
-gpu_name=$(echo "$xml_output" | xmlstarlet sel -t -v "//gpu/product_name")
-driver_version=$(echo "$xml_output" | xmlstarlet sel -t -v "//driver_version")
-cuda_version=$(echo "$xml_output" | xmlstarlet sel -t -v "//cuda_version")
-number_gpu=$(echo "$xml_output" | xmlstarlet sel -t -v "//attached_gpus")
-gpu_memory=$(echo "$xml_output" | xmlstarlet sel -t -v "//fb_memory_usage/total")
-
-
-# Print the extracted values
-echo "GPU Name: $gpu_name"
-echo "number of GPU: $number_gpu"
-echo "Driver Version: $driver_version"
-echo "Cuda Version: $cuda_version"
-echo "GPU memory: $gpu_memory"
-}
-
-UPDATE_DONE=0
-clear
-displayStatus
 
 for package in "${missing_packages[@]}"; do
     installPackage $package
 done
 
-displayStatus
-getGPUInfo
+echo -e "${BLUE}Checking NVIDIA GPU and Docker configuration...${NC}"
+
+if command -v nvidia-smi &> /dev/null; then
+    echo -e "${GREEN}NVIDIA drivers are installed.${NC}"
+    nvidia-smi
+else
+    echo -e "${RED}NVIDIA drivers are not installed.${NC}"
+fi
+
+# Configuring Docker
+USER_NAME=$(whoami)
+
+group_exists() {
+    getent group docker > /dev/null 2>&1
+}
+
+if ! group_exists; then
+    echo "The docker group does not exist. Creating docker group..."
+    sudo groupadd docker
+    if [ $? -eq 0 ]; then
+        echo -e "${GREEN}Docker group created successfully.${NC}"
+    else
+        echo -e "${RED}Failed to create docker group. Please check your permissions.${NC}"
+        exit 1
+    fi
+else
+    echo -e "${GREEN}Docker group already exists.${NC}"
+fi
+
+if groups $USER_NAME | grep -qw docker; then
+    echo -e "${GREEN}$USER_NAME is already in the docker group.${NC}"
+else
+    echo "$USER_NAME is not in the docker group. Adding $USER_NAME to the docker group..."
+    sudo usermod -aG docker $USER_NAME
+    if [ $? -eq 0 ]; then
+        echo -e "${GREEN}$USER_NAME has been added to the docker group. Please log out and back in for this to take effect.${NC}"
+    else
+        echo -e "${RED}Failed to add $USER_NAME to the docker group. Please check your permissions.${NC}"
+    fi
+fi
--- nvidiaSetup.sh
+++ nvidiaSetup.sh
@@ -1,37 +1,6 @@
 # !/bin/bash
 #  This script simply configurs Nvidia drivers in a typical Linux environment.  
 
-clean_docker() {
-  sudo docker volume ls -q -f driver=nvidia-docker \
-  | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
-  sudo apt-get purge nvidia-docker
-  sudo apt-get remove docker docker-engine docker.io containerd runc
-}
-
-install_docker(){
-  sudo apt-get update
-  sudo apt-get install ca-certificates curl
-  sudo install -m 0755 -d /etc/apt/keyrings
-  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-  sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-  sudo apt update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin jq
-
-  sudo usermod  --append --groups docker $USER
-  sudo docker run hello-world
-  }
-
-simple_docker_config()
-{
-  sudo usermod  --append --groups docker $USER
-}
 
 install_nvidia_drivers(){
 
@@ -39,13 +8,10 @@ sudo apt install linux-headers-$(uname -r)
 sudo apt install pciutils 
 sudo apt install libvulkan1
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
-sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers/cuda-repo-ubuntu2204-12-2-local_12.2.0-535.54.03-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2204-12-2-local_12.2.0-535.54.03-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2204-12-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
-sudo apt-get -y install cuda
+sudo apt-get -y install cuda-toolkit-12-4
 
 }
 
@@ -60,14 +26,6 @@ sudo apt-get install -y nvidia-container-runtime
 sudo systemctl restart docker
 }
 
-nvidia_driver_check(){
-sudo docker run --gpus=all --rm nvidia/cuda:11.8.0-runtime-ubuntu20.04 nvidia-smi
-}
-
-#clean_docker #Very seldom is this step required, but it is included in the standard Heavy.AI documentation
-#install_docker #Most environments already have docker installed and a more straightforward Docker install tends to work.  This step is also outlined in Heavy.AI documentation, but is most often not needed
-simple_docker_config
 install_nvidia_drivers
 nvidia_docker_runtime
-echo "===>>>> Make sure you execute the following command before trying to run docker-compose"
-echo "newgrp docker"
+
