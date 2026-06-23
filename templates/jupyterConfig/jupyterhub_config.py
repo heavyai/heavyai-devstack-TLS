76f35b7ff90fc0984ad785b1db339522e332c81f
misiugodfrey <misiug@nvidia.com>
2026-06-23 14:55:48 -0700

    chore: remove NVIDIA SPDX header from JupyterHub config file
    jupyterhub_config.py is derived from the JupyterHub project (Jupyter
    Development Team, Modified BSD License). Exclude templates/jupyterConfig/
    from the copyright hook and restore the original Jupyter copyright notice.

--- templates/jupyterConfig/jupyterhub_config.py
+++ templates/jupyterConfig/jupyterhub_config.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
